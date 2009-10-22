370e7f068ead1f4d1d9d28fcaa82c5b52ede9600
Daniel Insley <dan@viviti.com>
2009-10-22 11:28:15 -0700

    generatator javascript now works better incase jquery has been put in compat mode and/or redefined

--- generators/tartarus/templates/public/javascripts/tartarus.jquery.js
+++ generators/tartarus/templates/public/javascripts/tartarus.jquery.js
@@ -1,6 +1,8 @@
-$(function(){
-  $('.togglable').each(function() {
-    var toggle_data = $(this).find('.toggle_data');
-    $(this).find('.toggle_link').click(function() { toggle_data.toggle(); })
+(function($) {
+  $(function() {
+    $('.togglable').each(function() {
+      var toggle_data = $(this).find('.toggle_data');
+      $(this).find('.toggle_link').click(function() { toggle_data.toggle(); })
+    });
   });
-});
+})(jQuery);
